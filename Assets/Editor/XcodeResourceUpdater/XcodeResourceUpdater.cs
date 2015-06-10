db8fe53d35143013addd2fcdcdef88047ce5bff3
STAR_ZERO <kenji.01.star@gmail.com>
2015-06-10 15:43:48 +0900

    Initial commit

---
+++ Assets/Editor/XcodeResourceUpdater/XcodeResourceUpdater.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.Diagnostics;
+
+public class XcodeResourceUpdater
+{
+
+    [PostProcessBuild]
+    public static void OnPostprocessBuild (BuildTarget buildTarget, string path)
+    {
+        UnityEngine.Debug.Log ("Path: " + path);
+        UnityEngine.Debug.Log ("BuildTarget: " + buildTarget);
+
+        if (buildTarget != BuildTarget.iOS) {
+            return;
+        }
+
+        Process proc = new Process ();
+        proc.StartInfo.FileName = "Assets/Editor/XcodeResourceUpdater/xcode_resource_updater";
+        proc.StartInfo.Arguments = path;
+        proc.StartInfo.UseShellExecute = false;
+        proc.StartInfo.RedirectStandardError = true;
+        proc.StartInfo.RedirectStandardOutput = true;
+        proc.OutputDataReceived += new DataReceivedEventHandler (OutputDataHandler);
+        proc.ErrorDataReceived += new DataReceivedEventHandler(ErrorDataHandler);
+
+        proc.Start ();
+        proc.BeginOutputReadLine ();
+        proc.BeginErrorReadLine ();
+        proc.WaitForExit (30000);
+        proc.Close ();
+    }
+
+    private static void OutputDataHandler (object sendingProcess, DataReceivedEventArgs outLine)
+    {
+        if (!string.IsNullOrEmpty (outLine.Data)) {
+            UnityEngine.Debug.LogWarning (outLine.Data);
+        }
+    }
+
+    private static void ErrorDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+    {
+        if (!string.IsNullOrEmpty (outLine.Data)) {
+            UnityEngine.Debug.LogError (outLine.Data);
+        }
+    }
+}
